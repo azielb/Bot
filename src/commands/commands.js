46a7f432b1d28b789e79e68955e336ced2312c2c
azielb <superboy282000@gmail.com>
2021-05-07 22:33:59 -0400

    Added aliases

--- src/commands/commands.js
+++ src/commands/commands.js
@@ -2,12 +2,14 @@ require("dotenv").config();
 
 module.exports = {
     name: "commands",
+    aliases: ['cmds'],
     async execute(client, message) {
         const id = message.author.id;
         var STR = "";
         for (const [key, value] of client.commands) {
             STR += `${process.env.PREFIX}${key}\n`
         }
+        
         client.users.cache.get(id).send(STR);
     }
 }
--- src/events/guild/message.js
+++ src/events/guild/message.js
@@ -6,7 +6,7 @@ module.exports = (discord, client, message) => {
 
     const args = message.content.slice(process.env.PREFIX.length).split(/ +/);
     const cmd_Name = args.shift().toLowerCase();
-    const command = client.commands.get(cmd_Name);
+    const command = client.commands.get(cmd_Name) || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(cmd_Name));
 
     if (command) command.execute(client, message, args, discord);
 }
