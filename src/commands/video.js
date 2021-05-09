ba96066da8bdf591ff2c4b8281777a361d6308ec
azielb <superboy282000@gmail.com>
2021-05-09 16:12:15 -0400

    Update video.js

--- src/commands/video.js
+++ src/commands/video.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 
 const videos = {
-    clown: "https://cdn.discordapp.com/attachments/586713238016622593/840771655261487134/clown.mp4",
+    clown: "https://cdn.discordapp.com/attachments/764447332288561152/841022684053635072/Clown.mov",
     serious: "https://cdn.discordapp.com/attachments/586713238016622593/840771120278011964/serious.mp4",
     smh: "https://cdn.discordapp.com/attachments/764447332288561152/836960557915308042/video0.mov",
 }
@@ -13,7 +13,7 @@ module.exports = {
     async execute(client, message, discord, args) {
         const video = args.join(" ").toLowerCase();
         const avatar = await client.users.cache.get(client.user.id).avatarURL();
-        
+
         message.channel.send(videos[`${video}`]).catch(() => {
             const embed = new discord.MessageEmbed()
                 .setColor('#FF0000')
