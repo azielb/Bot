b0f88b2c40e2f5cf9dd8dea5c1a584e5ca3a9f31
azielb <superboy282000@gmail.com>
2021-05-08 22:46:00 -0400

    Update monkey.js

--- src/commands/monkey.js
+++ src/commands/monkey.js
@@ -1,11 +1,10 @@
 const Scraper = require('images-scraper');
-
+const max_images = 1000;
 const google = new Scraper({
     puppeteer: {
         headless: true
     }
 })
-
 const clamp = function(num, min, max) {
     return Math.min(Math.max(num, min), max)
 }
@@ -14,7 +13,7 @@ module.exports = {
     name: "monkey",
     description: "Sends a random monkey image to a channel",
     async execute(_, message) {
-        google.scrape('monkey', 1000).then((results) => {
+        google.scrape('monkey', max_images).then((results) => {
             const index = clamp(Math.floor(Math.random() * results.length), 0, results.length - 1);
             message.channel.send(results[index].url);
         }).catch(() => {
