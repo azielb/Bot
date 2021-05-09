983ca7a74b855ab8e11581d3a1392f46b3f461d5
azielb <superboy282000@gmail.com>
2021-05-08 23:02:12 -0400

    Update monkey.js

--- src/commands/monkey.js
+++ src/commands/monkey.js
@@ -1,10 +1,12 @@
 const Scraper = require('images-scraper');
 const max_images = 1000;
+const ERROR_MESSAGE = "Failed to retrieve the image.";
 const google = new Scraper({
     puppeteer: {
         headless: true
     }
 })
+
 const clamp = function(num, min, max) {
     return Math.min(Math.max(num, min), max)
 }
@@ -15,9 +17,13 @@ module.exports = {
     async execute(_, message) {
         google.scrape('monkey', max_images).then((results) => {
             const index = clamp(Math.floor(Math.random() * results.length), 0, results.length - 1);
-            message.channel.send(results[index].url);
+            if (!typeof(results[index].url) === "string") {
+                message.channel.send(ERROR_MESSAGE);
+            } else {
+                message.channel.send(results[index].url);
+            }
         }).catch(() => {
-            message.channel.send("Failed to retrieve the image.")
+            message.channel.send(ERROR_MESSAGE);
         })
     }
 } 
