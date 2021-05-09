04ba404193400454fcb6283334ff493367a6f7cb
azielb <superboy282000@gmail.com>
2021-05-09 13:52:48 -0400

    Update monkey.js

--- src/commands/monkey.js
+++ src/commands/monkey.js
@@ -1,7 +1,8 @@
 const Scraper = require('images-scraper');
-const max_images = 1000;
+const max_images = 10000;
 const ERROR_MESSAGE = "Failed to retrieve the image.";
 const results = new Array();
+var ready = false;
 
 new Scraper({
     puppeteer: {
@@ -9,6 +10,7 @@ new Scraper({
     }
 }).scrape('monkey', max_images).then((r) => {
     console.log("Finished scraping for monkey images.");
+    ready = true;
     for (result of r) {
         results.push(result);
     }
@@ -20,8 +22,14 @@ const clamp = function(num, min, max) {
 
 module.exports = {
     name: "monkey",
+    aliases: ['m'],
     description: "Sends a random monkey image to a channel",
     async execute(_, message) {
+        if (!ready) {
+            message.channel.send("Have not finished caching monkey images.");
+            return;
+        }
+
         const index = clamp(Math.floor(Math.random() * results.length), 0, results.length - 1);
         message.channel.send(results[index].url).catch(() => {
             message.channel.send(ERROR_MESSAGE);
