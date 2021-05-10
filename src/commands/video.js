a69754eac804e9f1dcdb8f2510c6fa04b35c2d6f
azielb <superboy282000@gmail.com>
2021-05-09 23:05:44 -0400

    Update video.js

--- src/commands/video.js
+++ src/commands/video.js
@@ -5,6 +5,7 @@ const videos = {
     serious: "https://cdn.discordapp.com/attachments/586713238016622593/840771120278011964/serious.mp4",
     smh: "https://cdn.discordapp.com/attachments/764447332288561152/836960557915308042/video0.mov",
     drip: "https://cdn.discordapp.com/attachments/586713238016622593/841118230457221150/drip.mp4",
+    why: "https://cdn.discordapp.com/attachments/764447332288561152/837080024733450240/video0.mp4",
 }
 
 module.exports = {
