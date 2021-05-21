05de50a776f4a894adee07c69bec8df6de2abe5a
azielb <superboy282000@gmail.com>
2021-05-21 08:59:25 -0400

    Update message.js

--- src/events/guild/message.js
+++ src/events/guild/message.js
@@ -7,6 +7,17 @@
     )
 */
 
+const clamp = function(num, min, max) {
+    return Math.min(Math.max(num, min), max)
+}
+
+const messages = [
+    "You dare speak about my master?",
+    "What do you want?",
+    "https://media.discordapp.net/attachments/565643666828427284/844543541798633522/image0.gif",
+    "https://cdn.discordapp.com/attachments/764447332288561152/844615536808558622/image0.gif",
+]
+
 module.exports = (discord, client, message) => {
     if (message.author.bot) return
     if (message.content.startsWith(process.env.PREFIX)) {
@@ -16,6 +27,13 @@ module.exports = (discord, client, message) => {
     
         if (command) command.execute(client, message, discord, args);
     } else if (message.content.toLowerCase().includes(process.env.OWNER_NAME)) {
-        message.reply("You dare speak about my master?");
+
+        if (message.author.id === process.env.OWNER_ID) {
+            message.reply("Hello master")
+            return
+        }
+
+        const index = clamp(Math.floor(Math.random() * messages.length), 0, messages.length - 1);
+        message.reply(messages[index]);
     }
 }
