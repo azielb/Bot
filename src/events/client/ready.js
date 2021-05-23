985a46cd902f36e2f9ee28648b4ec228ce9659d6
azielb <superboy282000@gmail.com>
2021-05-23 11:54:25 -0400

    Update ready.js

--- src/events/client/ready.js
+++ src/events/client/ready.js
@@ -1,9 +1,12 @@
 module.exports = (client) => {
     console.log(`${client.user.username} [ONLINE]`);
     client.user.setStatus('online');
-    client.user.setActivity(`${process.env.PREFIX}commands | ${process.env.PREFIX}help`, {
+    client.user.setActivity('DN', {
         type: "WATCHING",
     });
+    // client.user.setActivity(`${process.env.PREFIX}commands | ${process.env.PREFIX}help`, {
+    //     type: "WATCHING",
+    // });
     
     //Sets bot avatar to whatever my avatar is at the time.
     client.users.fetch(process.env.OWNER_ID).then((me) => {
