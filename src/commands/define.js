435e24c38b7a0c89dd1f41667964319223b3bd91
azielb <superboy282000@gmail.com>
2021-05-26 15:21:57 -0400

    Update

--- src/commands/define.js
+++ src/commands/define.js
@@ -4,14 +4,19 @@ module.exports = {
     description: "Gets a word's definition",
     async execute(client, message, args) {
         if (!args.length) return message.channel.send('Please enter a word to define!');
-
-        client.fetch(`https://www.dictionaryapi.com/api/v3/references/collegiate/json/${args[0]}?key=${process.env.WEBSTER_API_KEY}`).then(async response => {
+        
+        args = args.join(' ');
+        
+        client.fetch(`https://www.dictionaryapi.com/api/v3/references/collegiate/json/${args}?key=${process.env.WEBSTER_API_KEY}`).then(async response => {
             const data = await response.json()
+
+            if (!data[0] || !data[0].shortdef) return message.channel.send(`No definitions found for: \`${args}\``)
+
             var count = 1;
 
             const embed = new client.discord.MessageEmbed()
                 .setColor('#FFFF00')
-                .setTitle(`Definitions for \`${args[0]}\``)
+                .setTitle(`Definitions for: \`${args}\``)
                 .setAuthor(process.env.BOT_NAME, client.avatar)
                 .setTimestamp()
 
--- src/commands/urban-define.js
+++ src/commands/urban-define.js
@@ -4,14 +4,19 @@ module.exports = {
     description: "Gets a word's definition from urban dictionary",
     async execute(client, message, args) {
         if (!args.length) return message.channel.send('Please enter a word to define!');
-
-        client.fetch(`http://api.urbandictionary.com/v0/define?term=${args[0]}`).then(async response => {
-            const data = await response.json()
+       
+        args = args.join(' ');
+        
+        client.fetch(`http://api.urbandictionary.com/v0/define?term=${args}`).then(async response => {
+            const data = await response.json();
+            
+            if (!data.list || !data.list.length) return message.channel.send(`No definitions found for: \`${args}\``)
+    
             var count = 1;
-
+            
             const embed = new client.discord.MessageEmbed()
                 .setColor('#FFFF00')
-                .setTitle(`Urban Dictionary Definitions for: \`${args[0]}\``)
+                .setTitle(`Urban Dictionary Definitions for: \`${args}\``)
                 .setAuthor(process.env.BOT_NAME, client.avatar)
                 .setTimestamp()
 
